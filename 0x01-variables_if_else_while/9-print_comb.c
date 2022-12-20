d737d0630777eb6322b4cb3f1e2e417cbcfb923c
megter <megter0830@gmail.com>
2022-12-20 05:16:29 -0800

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,16 +7,15 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n  < 100; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
-		putchar(n + '0');
-		if (n < 9)
+		putchar(n);
+		if (n != '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
