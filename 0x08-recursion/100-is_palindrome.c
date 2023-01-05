10674642346a80baff992e6fc79634ba9d9c29af
megter <megter0830@gmail.com>
2023-01-05 09:36:49 -0800

    7. They say we only use a fraction of our brain's true potential. Now that's when we're awake.

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -22,11 +22,11 @@ int is_palindrome(char *s)
  * Return: length of the string
  */
 int _strlen_recursion(char *s)
-
+{
 	if (*s == '\0')
-	return (0);
+		return (0);
 	return (1 + _strlen_recursion(s + 1));
-	}
+}
 
 /**
  * check_pal - checks the characters
